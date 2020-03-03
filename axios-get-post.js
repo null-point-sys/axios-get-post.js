b0e4d538ebb5af37ee1251c7d8daab7b8b22757d
null-point-sys <jaalza@gmail.com>
2020-03-03 08:58:17 -0500

    Update axios-get-post.js

--- axios-get-post.js
+++ axios-get-post.js
@@ -1,12 +1,12 @@
 var print_get = function(data) {
-	  var result = data.result;
-	  console.log(result);
-	  for (var prop in result) {
+	var result = data.result;
+	console.log(result);
+	/*for (var prop in result) {
 		if (result.hasOwnProperty(prop)) {
 		  console.log(result[prop]);
 		}
-	  }
-	};
+	  }*/
+};
 
 	async function makeRequest(requestType, url, obj) {
 	  if (requestType === "post") {
